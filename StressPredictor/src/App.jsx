d1345a90150d516382849ace953a7a927927864b
lakshitasisodia <hrxss888@gmail.com>
2025-12-08 13:24:43 +0530

    initial commit

---
+++ StressPredictor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import StressPredictorDemo from './assets/StressPredictorDemo'
+
+function App() {
+  
+
+  return (
+    <>
+      <StressPredictorDemo/>
+    </>
+  )
+}
+
+export default App
